1de6b503eca22287ca1805219470fc9675657de9
ThomBrice <thomas.brice.59@gmail.com>
2016-10-13 09:47:32 +0200

    Branch Maps

--- app/src/main/java/com/example/isen/noroughapk/MainActivity.java
+++ app/src/main/java/com/example/isen/noroughapk/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.isen.noroughapk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
