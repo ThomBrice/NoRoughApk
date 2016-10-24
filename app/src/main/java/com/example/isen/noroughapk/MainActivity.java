f92cd52d7c0e306ed187d1df112fc662e1ea14c4
ThomBrice <thomas.brice.59@gmail.com>
2016-10-24 15:04:52 +0200

    Architecture de base fonctionnelle. Ouverture du fragment "Activity" dés le début. Plusieurs BlankFragment implémentés.

--- app/src/main/java/com/example/isen/noroughapk/MainActivity.java
+++ app/src/main/java/com/example/isen/noroughapk/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.isen.noroughapk;
 
-
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,25 +10,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-       setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.playButton);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
+        setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -39,6 +32,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.getMenu().getItem(0).setChecked(true);
+        navigationView.getMenu().performIdentifierAction(R.id.nav_activite,0);
     }
 
     @Override
@@ -85,11 +80,26 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_historique) {
+        if (id == R.id.nav_activite) {
+            ActivityFragment activityFragment = new ActivityFragment();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_main, activityFragment)
+                    .commit();
+        }else if (id == R.id.nav_historique) {
+            HistoryFragment historyFragment = new HistoryFragment();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_main, historyFragment)
+                    .commit();
         } else if (id == R.id.nav_statistique) {
-
+            StatisticFragment statisticFragment = new StatisticFragment();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_main , statisticFragment)
+                    .commit();
         } else if (id == R.id.nav_swing) {
-
+            AnalyseFragment analyseFragment = new AnalyseFragment();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_main, analyseFragment)
+                    .commit();
         } else if (id == R.id.nav_amis) {
 
         } else if (id == R.id.nav_mon_compte) {
